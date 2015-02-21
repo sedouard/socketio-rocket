d37a56b6bd8daf7be1c3fd6ffff81102d6a4417a
sedouard <steven.edouard1@gmail.com>
2015-02-20 21:37:30 -0800

    added rocket launch code

--- Finished/server.js
+++ Finished/server.js
@@ -60,10 +60,12 @@ io.sockets.on('connection', function (socket) {
     	var stream = twitter.stream('statuses/filter', { track: "#" + data.track });
     	var count = 0;
     	var goal = data.goal;
+    	var fired = false;
 	    stream.on('tweet', function (tweet) {
 	    	count++;
 	        socket.emit('blast', {msg:"<span style=\"color:red !important\">" + tweet.text + "!</span>", count: count});
-	        if(count >= goal){
+	        if(count >= goal && !fired){
+	        	fired = true;
 	        	//stop the stream
 	        	console.log('GOAL REACHED! TODO - Fire Rocket!');
 	        	stream.stop();
@@ -80,6 +82,7 @@ io.sockets.on('connection', function (socket) {
 	    //stop tracking tweets after disconnecting
 	    socket.on('disconnect', function(socket){
 	    	console.log("!!!!CLIENT DISCONNECTED!!!");
+	    	
 	    	stream.stop();
 
 			
@@ -92,19 +95,19 @@ io.sockets.on('connection', function (socket) {
 function fireRocket(cb){
 
 	//TODO: NEVER place your actual spark core access_token in your file
-  unirest.post('https://api.spark.io/v1/devices/53ff6e066667574833512467/cycleRelay')
-  .send('access_token=a60207c84d396e58510aae5b91f5bbfb0dccb9a7')
+  unirest.post('https://api.spark.io/v1/devices/54ff6c066672524825221267/cycleRelay')
+  .send('access_token=7c68b236b798a024b418a568f4d7cc06eb422ab6')
   .send('params=r1,1500')
   .end(function(response){
 
     if(response.error){
       //something went wrong!
       console.error(response.error);
-      cb(response.error);
+      return cb(response.error);
     }
 
     //rocket triggered! (make sure its on and connected)
-    cb(null);
+    return cb(null);
   });
 
 
